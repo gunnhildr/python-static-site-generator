c0c8eebf75a31eacb3ee53b12ee2de6dfeef7052
vkholomenyuk <vkholomenyuk@gmail.com>
2021-12-27 15:25:49 +0100

    module3 fixes

--- ssg/content.py
+++ ssg/content.py
@@ -4,12 +4,12 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
-        _, fm, content = cls.__regex.split(string=string, maxsplit=2)
+        _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
